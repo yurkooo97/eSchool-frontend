83668021689d8ece405240bef633a06c59ff9876
aharxict <it.ahapx@gmail.com>
2018-11-07 14:30:46 +0200

    added menu and hometask components for journal

--- src/app/journal/journal.module.ts
+++ src/app/journal/journal.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { JournalRoutingModule } from './journal-routing.module';
 import { JournalComponent } from './journal/journal.component';
+import { MenuComponent } from './journal/menu/menu.component';
+import { HometaskComponent } from './journal/hometask/hometask.component';
 
 @NgModule({
   imports: [
     CommonModule,
     JournalRoutingModule
   ],
-  declarations: [JournalComponent]
+  declarations: [JournalComponent, MenuComponent, HometaskComponent]
 })
 export class JournalModule { }
---
+++ src/app/journal/journal/hometask/hometask.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hometask',
+  templateUrl: './hometask.component.html',
+  styleUrls: ['./hometask.component.scss']
+})
+export class HometaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
