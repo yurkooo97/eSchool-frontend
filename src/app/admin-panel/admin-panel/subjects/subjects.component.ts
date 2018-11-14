a65c3eb37ecc2a4b0f5b4dd61d694588073c3487
Your Name <you@example.com>
2018-11-14 21:12:51 +0200

    added validation to subject component and created method onSubmit

--- src/app/admin-panel/admin-panel/subjects/subjects.component.ts
+++ src/app/admin-panel/admin-panel/subjects/subjects.component.ts
@@ -1,77 +1,132 @@
-import { Component, OnInit } from '@angular/core';
-import { AdminSubjectsService } from 'src/app/services/admin-subjects.service';
-import {Subject} from 'src/app/models/subjects.model';
+import { Component, OnInit } from "@angular/core";
+import { AdminSubjectsService } from "src/app/services/admin-subjects.service";
+import { Subject } from "src/app/models/subjects.model";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-subjects',
-  templateUrl: './subjects.component.html',
-  styleUrls: ['./subjects.component.scss']
+  selector: "app-subjects",
+  templateUrl: "./subjects.component.html",
+  styleUrls: ["./subjects.component.scss"]
 })
 export class SubjectsComponent implements OnInit {
+  subjectForm: FormGroup;
 
-	loading: boolean;
+  submitted = false;
 
-	displayDialog: boolean;
+  loading: boolean;
 
-	subject: any;
+  displayDialog: boolean;
 
-	selectedSubject: Subject;
+  subject: any;
 
-	newSubject: boolean;
+  selectedSubject: Subject;
 
-	subjects: Subject[];
+  newSubject: boolean;
 
-	cols: Array<object>;
+  subjects: Subject[];
 
-	constructor(private _subjectsService: AdminSubjectsService) {}
+  cols: Array<object>;
 
-	ngOnInit() {
-		this.loading = true;
-		this._subjectsService.getSubjectsList().subscribe(data => this.subjects = data);
-		this.cols = [{
-				field: 'subjectName',
-				header: 'Назва'
-			},
-			{
-				field: 'subjectDescription',
-				header: 'Опис'
-			}
-		];
-		this.loading = false;
-	}
+  constructor(
+    private _subjectsService: AdminSubjectsService,
+    private formBuilder: FormBuilder
+  ) {}
 
-	showDialogToAdd() {
-		this.newSubject = true;
-		this.subject = {};
-		this.displayDialog = true;
-	}
+  ngOnInit() {
+    this.loading = true;
+    this._subjectsService
+      .getSubjectsList()
+      .subscribe(data => (this.subjects = data));
+    this.cols = [
+      {
+        field: "subjectName",
+        header: "Назва"
+      },
+      {
+        field: "subjectDescription",
+        header: "Опис"
+      }
+    ];
+    this.loading = false;
+    this.subjectForm = this.formBuilder.group({
+      subjectName: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+          Validators.pattern("[А-ЯІЇЄҐ]([А-ЯІЇЄҐ]*[а-яіїєґ]*[' -]?)+")
+        ])
+      ],
+      subjectDescription: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(300)
+        ])
+      ]
+    });
+  }
 
-	create() {
-		this.displayDialog = false;
-		this._subjectsService.postSubject(this.subject).subscribe(
-			subject => this.subjects.push(subject),
-			err => console.error(err)
-		);
-	}
+  get f() {
+    return this.subjectForm.controls;
+  }
 
-	save() {
-		this.displayDialog = false;
-		this._subjectsService.putSubject(this.subject).subscribe(
-			subject => {
-				let subjects = [...this.subjects];
-				subjects[this.subjects.indexOf(this.selectedSubject)] = subject;
-				this.subjects = subjects;
-			},
-			err => console.error(err)
-		);
-		this.subject = null;
-	}
+  onSubmit() {
+    this.submitted = true;
+    this.subjectForm.setValue({
+      subjectName: this.subject.subjectName,
+      subjectDescription: this.subject.subjectDescription
+	});
+	
+    if (this.subjectForm.invalid) {
+      return;
+    }
+    if (this.newSubject) {
+      this.create();
+    } else {
+      this.save();
+    }
+  }
 
-	onRowSelect(rowData) {
-		this.newSubject = false;
-        this.subject = {
-            ...rowData
-        };
-		this.displayDialog = true;
-	}
+  showDialogToAdd() {
+    this.submitted = false;
+    this.newSubject = true;
+    this.subject = {};
+    this.displayDialog = true;
+  }
+
+  create() {
+    this.displayDialog = false;
+    this._subjectsService
+      .postSubject(this.subject)
+      .subscribe(
+        subject => this.subjects.push(subject),
+        err => console.error(err)
+      );
+  }
+
+  save() {
+    this.displayDialog = false;
+    this._subjectsService.putSubject(this.subject).subscribe(
+      subject => {
+        const subjects = [...this.subjects];
+        subjects[this.subjects.indexOf(this.selectedSubject)] = subject;
+		    this.subjects = subjects;
+	    	this.ngOnInit();
+      },
+      err => console.error(err)
+    );
+    this.subject = null;
+  }
+
+  onRowSelect(rowData) {
+    this.submitted = false;
+    this.newSubject = false;
+    this.subject = {
+      ...rowData
+    };
+    this.displayDialog = true;
+  }
 }
