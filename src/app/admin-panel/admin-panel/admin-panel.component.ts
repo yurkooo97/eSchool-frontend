468d34cc7b07de5cf32681191866d8e60706cd0a
Malyna Marian <malina.marian@gmail.com>
2018-11-14 12:48:35 +0200

    attached teacher to jornal v.1.0.1

--- src/app/admin-panel/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel/admin-panel.component.ts
@@ -25,5 +25,3 @@ export class AdminPanelComponent implements OnInit {
     }];
   }
 }
-
-
--- src/app/services/http-attach-teacher.service.ts
+++ src/app/services/http-attach-teacher.service.ts
@@ -10,11 +10,7 @@ import { AttachedTeacher } from '../models/attached-teacher.model';
   providedIn: 'root'
 })
 export class HttpAttachTeacherService {
-  private jwtToken = {
-    headers: new HttpHeaders({
-      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsIlJvbGVzIjp7ImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifSwiZXhwIjoxNTQyMTQ4NDI2LCJpYXQiOjE1NDIxNDQ4MjYsImp0aSI6IjIzMSJ9.PhrQhbtgCdZIll4CkjkXFjNuoSpXbLVbBCNd-kxvPiG35DY_dBAHCLcXwNSebwOKYcMqcHc1-jLyEOt9dcxMWg'
-    })
-  };
+
   private urlTeachers = 'https://fierce-shore-32592.herokuapp.com/teachers';
   private urlSubjects = 'https://fierce-shore-32592.herokuapp.com/subjects';
   private urlClases = 'https://fierce-shore-32592.herokuapp.com/classes';
@@ -22,17 +18,15 @@ export class HttpAttachTeacherService {
   constructor(private http: HttpClient) { }
 
   getTeachers(): Observable<Teacher[]> {
-    return this.http.get<Teacher[]>(this.urlTeachers, this.jwtToken);
+    return this.http.get<Teacher[]>(this.urlTeachers);
   }
   getSubjects(): Observable<Subject[]> {
-    return this.http.get<Subject[]>(this.urlSubjects, this.jwtToken);
+    return this.http.get<Subject[]>(this.urlSubjects);
   }
   getClasses(): Observable<Group[]> {
-    return this.http.get<Group[]>(this.urlClases, this.jwtToken);
+    return this.http.get<Group[]>(this.urlClases);
   }
   postAttachTeacher (object: AttachedTeacher): Observable<any> {
-    return this.http.post<any>(`${this.urlTeachers}/${object.teacherId}/classes/${object.classId}/subjects/${object.subjectId}/journal`, object, this.jwtToken);
+    return this.http.post<any>(`${this.urlTeachers}/${object.teacherId}/classes/${object.classId}/subjects/${object.subjectId}/journal`, object);
   }
 }
-
-
