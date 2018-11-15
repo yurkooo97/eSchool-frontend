d4ab91d528354448aa3b272dac63f67d26388eb7
Your Name <you@example.com>
2018-11-15 19:59:22 +0200

    added headers to interceptor and removed from services

--- src/app/services/admin-students.service.ts
+++ src/app/services/admin-students.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Student } from '../models/students.model';
 import { Class_ } from '../models/classesForStudents.model';
 import { Observable } from 'rxjs';
@@ -8,34 +8,26 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class StudentsService {
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-      'Access-Control-Allow-Headers': 'accept, content-type',
-      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsIlJvbGVzIjp7ImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifSwiZXhwIjoxNTQxOTc1ODE1LCJpYXQiOjE1NDE5NzIyMTUsImp0aSI6IjIzMSJ9.a0D-efTGiHC4wyb-clgsvNQOPFGtX99p8Caqr0T_2EcPWldvsIoScpDcGOzg57qKsWJHmir0Gdkx24awO1JTJg'
-    })
-  };
-
   private classesUrl = 'https://fierce-shore-32592.herokuapp.com/classes';
   private studentsUrl = 'https://fierce-shore-32592.herokuapp.com/students';
-  private studentByClassUrl = 'https://fierce-shore-32592.herokuapp.com/students/classes/';
+  private studentByClassUrl =
+    'https://fierce-shore-32592.herokuapp.com/students/classes/';
 
-  constructor (private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getClasses() {
-    return this.http.get<Class_[]>(this.classesUrl, this.httpOptions);
+    return this.http.get<Class_[]>(this.classesUrl);
   }
 
   getStudents(idClass): Observable<Student[]> {
-    return this.http.get<Student[]>(this.studentByClassUrl + idClass, this.httpOptions);
+    return this.http.get<Student[]>(this.studentByClassUrl + idClass);
   }
 
   addStudent(student: Student): Observable<Student> {
-    return this.http.post<Student>(this.studentsUrl, student, this.httpOptions);
+    return this.http.post<Student>(this.studentsUrl, student);
   }
 
   changeStudent(student: Student): Observable<Student> {
-    return this.http.put<Student>(`${this.studentsUrl}/${student.id}`, student, this.httpOptions);
+    return this.http.put<Student>(`${this.studentsUrl}/${student.id}`, student);
   }
 }
--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -1,17 +1,27 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor} from '@angular/common/http';
-import { AuthenticationService} from './authentication.service'
+import { HttpInterceptor, HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService implements HttpInterceptor{
-  
-  constructor(private authService: AuthenticationService,
-    private router: Router) { }
-  
+export class TokenInterceptorService implements HttpInterceptor {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE',
+      'Access-Control-Allow-Headers': 'accept, content-type'
+    })
+  };
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
   intercept(req, next) {
+    req = req.clone(this.httpOptions);
     const token = this.authService.getToken();
     if (token == null) {
       if (this.router.url !== '/login' && this.router.url !== '/login/') {
@@ -21,7 +31,8 @@ export class TokenInterceptorService implements HttpInterceptor{
     }
     const tokenizedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
-    })
-    return next.handle(tokenizedReq)
+    });
+    console.log(req);
+    return next.handle(tokenizedReq);
   }
 }
