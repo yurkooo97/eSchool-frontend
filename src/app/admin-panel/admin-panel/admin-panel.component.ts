7fba3ad927106ddf405e7bb61cc180c383162502
NatalyaTretyak <nmtretyak@gmail.com>
2018-11-17 14:47:35 +0200

    add changed admin-panel.component.ts

--- src/app/admin-panel/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel/admin-panel.component.ts
@@ -13,7 +13,6 @@ export class AdminPanelComponent implements OnInit {
 
   ngOnInit() {
     this.items = [{
-<<<<<<< HEAD
       label:'Навігація',
       items:[
 				{ label: 'Учні', icon: 'pi  pi-users', routerLink: ['students']},
@@ -22,19 +21,7 @@ export class AdminPanelComponent implements OnInit {
 				{label: 'Класи', icon: 'pi pi-folder', routerLink: ['groups']},
 				{ label: 'Розклад уроків', icon: 'pi pi-info', routerLink: ['class-schedule']},
         {label: 'Створити новий навчальний рік', icon:'pi pi-fw pi-plus', routerLink: ['new-studing-year']}
-    ]
-  }]
-=======
-      label: 'Навігація',
-      items: [
-        { label: 'Учні', icon: 'pi  pi-users', routerLink: ['students'] },
-        { label: 'Учителі', icon: 'pi pi-user-minus', routerLink: ['teachers'] },
-        { label: 'Предмети', icon: 'pi pi-pencil', routerLink: ['subjects'] },
-        { label: 'Класи', icon: 'pi pi-folder', routerLink: ['groups'] },
-        { label: 'Створити новий навчальний рік', icon: 'pi pi-fw pi-plus', routerLink: ['new-studing-year'] },
-        { label: 'Додати вчителя до журналу', icon: 'pi pi-user-plus', routerLink: ['attach-teacher'] }
-      ]
-    }];
->>>>>>> 53eb145c64c009adcba7c0a3d39eec51c6e0330d
-  }
+    	]
+  	}]
+	}
 }
