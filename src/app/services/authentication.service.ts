83f9199147c3bf7bfae07c08f46538b9fce1aa4e
Taras Ivanytskyi <taras.ivanytskii@gmail.com>
2018-11-17 12:34:22 +0200

    Changed service to get token from Header

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,39 +5,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  public Url:string="https://fierce-shore-32592.herokuapp.com/signin";
-  private token: string;
+  public Url: string = 'https://fierce-shore-32592.herokuapp.com/signin';
 
-  constructor(private httpClient:HttpClient) {
-    this.token = localStorage.getItem('authToken');
+  constructor(private httpClient: HttpClient) {
   }
-  
-  login (userName: string, password: string) {
-    let userData = {username: userName, password: password};
-    return this.httpClient.post(this.Url, userData)
+
+  login(userName: string, password: string) {
+    let userData = { username: userName, password: password };
+    return this.httpClient.post(this.Url, userData, { observe: 'response' })
       .map((response: any) => {
-        if (response.status.code == 200){
-          this.token = response.data.token;
-          localStorage.setItem('authToken', this.token);
+        if (response.status === 200 || response.status === 204) {
+          localStorage.setItem('authToken', response.headers.get('Authorization'));
         }
         return response;
       });
   }
 
-  public isAuthenticated(): boolean {
-    return this.token !== null;
-  }
-
   getToken() {
-    return this.token;
+    return localStorage.getItem('authToken');
   }
 
   logOut() {
-    this.token = null;
     localStorage.removeItem('authToken');
   }
-  
+
   loggedIn() {
-    return !!localStorage.getItem('authToken')
+    return !!localStorage.getItem('authToken');
   }
+
 }
--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -1,27 +1,36 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor} from '@angular/common/http';
-import { AuthenticationService} from './authentication.service'
+import { HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService implements HttpInterceptor{
-  
+export class TokenInterceptorService implements HttpInterceptor {
+
   constructor(private authService: AuthenticationService,
     private router: Router) { }
-  
+
   intercept(req, next) {
     const token = this.authService.getToken();
-    if (token == null) {
+    if (token == null || token === '') {
       if (this.router.url !== '/login' && this.router.url !== '/login/') {
         this.router.navigate(['/login']);
       }
       return next.handle(req);
     }
     const tokenizedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    })
+      headers: req.headers.set('Authorization', token)
+    });
     return next.handle(tokenizedReq)
+      .catch((errorResponse: HttpErrorResponse) => {
+        if (errorResponse.status === 401 || errorResponse.status === 400) {
+          this.authService.logOut();
+          this.router.navigate(['/login']);
+        }
+        return Observable.throw(errorResponse);
+
+      });
   }
 }
