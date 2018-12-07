0a765572eaedb6437734b7ead404a7d946bf43a0
Yurii Krupa <yurii.krupa.dev@gmail.com>
2018-12-07 11:11:39 +0200

    Feature/user profile rework (#64)
    * Reworked user profile

--- src/app/services/teachers.service.ts
+++ src/app/services/teachers.service.ts
@@ -3,45 +3,52 @@ import { HttpClient } from '@angular/common/http';
 import { Iteachers } from '../models/teachers';
 import { Observable, BehaviorSubject } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class TeachersService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   private calendarSource = new BehaviorSubject<object>({
     firstDayOfWeek: 1,
     dayNames: ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота'],
     dayNamesShort: ['Нед', 'Пон', 'Вів', 'Сер', 'Чет', 'П\'ят', 'Суб'],
     dayNamesMin: ['нд', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
-    monthNames: [ 'Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень',
-     'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень' ],
-    monthNamesShort: [ 'Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Гру' ],
+    monthNames: ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень',
+      'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'],
+    monthNamesShort: ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Гру'],
     today: 'Сьогодні',
     clear: 'Clear'
   });
   currentCalendar = this.calendarSource.asObservable();
-   formatDate(date) {
-      const d = new Date(date);
-      const  year = d.getFullYear();
-      let  month = '' + (d.getMonth() + 1);
-      let  day = '' + d.getDate();
-      if (month.length < 2) { month = '0' + month; }
-      if (day.length < 2) { day = '0' + day; }
-      return [year, month, day].join('-');
+  formatDate(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    if (month.length < 2) { month = '0' + month; }
+    if (day.length < 2) { day = '0' + day; }
+    return [year, month, day].join('-');
   }
   public getTeachers(): Observable<Iteachers[]> {
     return this.http.get<Iteachers[]>('teachers')
       .map((response: any) => {
         return response.data;
       });
-    }
+  }
   public postTeacher(teacher: Iteachers): Observable<Iteachers> {
-   return this.http.post<Iteachers>('teachers', teacher)
+    return this.http.post<Iteachers>('teachers', teacher)
       .map((response: any) => {
         return response.data;
       });
   }
   public putTeacher(teacher: Iteachers): Observable<Iteachers> {
     return this.http.put<Iteachers>(`/admin/teachers/${teacher.id}`, teacher)
-    .map((response: any) => {
+      .map((response: any) => {
+        return response.data;
+      });
+  }
+  public getTeacherBy(id: string): Observable<Iteachers> {
+    return this.http.get<Iteachers>(`teachers/${id}`).map((response: any) => {
       return response.data;
     });
   }
