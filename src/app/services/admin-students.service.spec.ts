a6f3c84b67082d04ca058c00aa68cd5b551b5327
MichaelValkiv <mishavalkiv@gmail.com>
2018-12-09 17:33:44 +0200

    Tests
    Tests for students service

---
+++ src/app/services/admin-students.service.spec.ts
@@ -0,0 +1,81 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { StudentsService } from './admin-students.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Student } from '../models/students.model';
+
+const mockStudents: Student[] = [
+  {
+    firstname: 'Степан',
+    lastname: 'Когут',
+    patronymic: 'Григорійович',
+    dateOfBirth: '1998-02-14',
+    classId: 0,
+    email: null,
+    phone: null,
+    login: 'sKohyt14',
+    id: 141,
+    avatar: null
+  },
+  {
+    firstname: 'Зеновій',
+    lastname: 'Запухляк',
+    patronymic: 'Віталійович',
+    dateOfBirth: '1999-12-31',
+    classId: 0,
+    email: null,
+    phone: null,
+    login: 'zZapukh31',
+    id: 151,
+    avatar: null
+  },
+  {
+    firstname: 'Тетяна',
+    lastname: 'Куца',
+    patronymic: 'Орестівна',
+    dateOfBirth: '1999-12-01',
+    classId: 0,
+    email: null,
+    phone: null,
+    login: 'tKutsa01',
+    id: 161,
+    avatar: null
+  }
+];
+
+describe('StudentsService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StudentsService]
+    })
+  );
+
+  it('should be created', () => {
+    const service: StudentsService = TestBed.get(StudentsService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should get all students from class', inject(
+    [StudentsService, HttpTestingController],
+    (service: StudentsService, backend: HttpTestingController) => {
+      service.getStudents(1).subscribe(students => {
+        expect(students).toEqual(mockStudents);
+      });
+      backend.expectOne({
+        method: 'GET',
+        url: 'students/classes/1'
+      }).flush(mockStudents);
+  }));
+
+  it('should post one student', inject(
+    [StudentsService, HttpTestingController],
+    (service: StudentsService, backend: HttpTestingController) => {
+      service.addStudent(mockStudents[2]).subscribe(student => {
+        expect(student).toEqual(mockStudents[2]);
+      });
+      backend.expectOne({
+        method: 'POST',
+        url: 'students'
+      }).flush(mockStudents[2]);
+    }));
+});
