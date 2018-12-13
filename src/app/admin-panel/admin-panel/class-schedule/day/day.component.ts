8ac1a948ada26f7d3cd37a5a8c905d99508a4652
NatalyaTretyak <nmtretyak@gmail.com>
2018-12-13 10:44:23 +0200

    added day component

---
+++ src/app/admin-panel/admin-panel/class-schedule/day/day.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.scss']
+})
+export class DayComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
