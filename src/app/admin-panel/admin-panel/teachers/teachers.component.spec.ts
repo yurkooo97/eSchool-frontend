3ed2bc80f8b0b1e0bdfda990d2cdcabdf304ecc7
Taras Kulchytskyi <kulchytskyitaras@gmail.com>
2018-12-15 16:37:51 +0200

    fixed view of calendars

--- src/app/admin-panel/admin-panel/teachers/teachers.component.spec.ts
+++ src/app/admin-panel/admin-panel/teachers/teachers.component.spec.ts
@@ -1,14 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TeachersComponent } from './teachers.component';
+import { TeachersService } from 'src/app/services/teachers.service';
+import { Observable } from 'rxjs';
+import { Iteachers } from 'src/app/models/teachers';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TeachersComponent', () => {
   let component: TeachersComponent;
   let fixture: ComponentFixture<TeachersComponent>;
+  let teachersService: TeachersService;
+  let spy: jasmine.Spy;
+  const mockTeachers: Iteachers[] = [
+    {
+      firstname: 'Михайло',
+      lastname: 'Грушевський',
+      patronymic: 'Сергійович',
+      dateOfBirth: '1866-09-29',
+      id: 0,
+      email: 'grush@gmail.com',
+      avatar: 'avatar2',
+      login: 'grush1',
+      phone: '0509754632',
+      newPass: '',
+      oldPass: ''
+    },
+    {
+      firstname: 'Іван',
+      lastname: 'Франко',
+      patronymic: 'Якимович',
+      dateOfBirth: '1856-08-27',
+      id: 1,
+      email: 'franko@gmail.com',
+      avatar: 'avatar1',
+      login: 'grush1',
+      phone: '0973159560',
+      newPass: '',
+      oldPass: ''
+    }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TeachersComponent ]
+      imports : [HttpClientModule],
+      declarations: [ TeachersComponent ],
+      providers: [TeachersService]
     })
     .compileComponents();
   }));
@@ -16,10 +52,20 @@ describe('TeachersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TeachersComponent);
     component = fixture.componentInstance;
+    teachersService = fixture.debugElement.injector.get(TeachersService);
+    spy = spyOn(teachersService, 'getTeachers').and.returnValue(Observable.of(mockTeachers));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call teachers service', () => {
+    component.ngOnInit();
+    expect(spy.calls.any()).toBeTruthy();
+  });
+  it('should set teachers', () => {
+    component.ngOnInit();
+    expect(component.teachers).toBeTruthy(mockTeachers);
   });
 });
