2a500820aac06dc3fd58bcc92d42b1d24e27c7f5
Malyna Marian <malina.marian@gmail.com>
2018-12-15 23:49:34 +0200

    added two tests for data-sharing.service.ts

--- src/app/services/data-sharing.service.spec.ts
+++ src/app/services/data-sharing.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataSharingService } from './data-sharing.service';
+import { Subject } from 'rxjs';
 
 describe('DataSharingService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -9,4 +10,20 @@ describe('DataSharingService', () => {
     const service: DataSharingService = TestBed.get(DataSharingService);
     expect(service).toBeTruthy();
   });
+
+  it('should be created showToasts', () => {
+    const service: DataSharingService = TestBed.get(DataSharingService);
+    expect(service.showToasts).toEqual(new Subject<Object>());
+  });
+
+  it('should be call notify', () => {
+    const service: DataSharingService = TestBed.get(DataSharingService);
+    expect(service.notify('success', 'Успішно виконано', 'Прив\'язку вчителя до журналу'))
+      .toEqual(service.showToasts.next({
+        severity: 'success',
+        summary: 'Успішно виконано',
+        detail: 'Прив\'язку вчителя до журналу'
+      }));
+  });
+
 });
