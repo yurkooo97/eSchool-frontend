a4cd72e40ee80792a4eda0f6582da6b9b7f989a5
NatalyaTretyak <nmtretyak@gmail.com>
2019-02-18 14:37:18 +0200

    added component for class schedule correcting

--- src/app/admin-panel/admin-panel/correct-schedule/correct-schedule.component.ts
+++ src/app/admin-panel/admin-panel/correct-schedule/correct-schedule.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassScheduleService } from 'src/app/services/class-schedule.service';
+import { Group } from 'src/app/models/group.model';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-correct-schedule',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./correct-schedule.component.scss']
 })
 export class CorrectScheduleComponent implements OnInit {
+  classes: Group[];
 
-  constructor() { }
+  constructor(private scheduleService: ClassScheduleService) {}
 
   ngOnInit() {
+    this.getClasses();
   }
 
+  // request to add a list of classes
+  getClasses(): void {
+    this.scheduleService.getClasses().subscribe(data => {
+      this.classes = data.filter(g => g.isActive);
+    });
+  }
 }
