f7b5debd115d1d2aeb47cbbdac948ea3e93f1ec7
Oleh.Kuryshko <Oleh.Kuryshko@gmail.com>
2020-08-09 23:35:46 +0300

    change dns name

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -14,7 +14,7 @@ import { throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  baseUrl = 'https://fierce-shore-32592.herokuapp.com';
+  baseUrl = 'http://lb-backend.devops.rebrain.srwx.net';
   allowedUrl = ['/login', '/login/', '/login/request-password', '/login/request-password/', '/login/password', '/login/password/'];
 
   constructor(
